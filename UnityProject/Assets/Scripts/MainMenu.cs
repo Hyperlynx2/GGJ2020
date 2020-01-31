49fa3ae063cb06b3fe9454029978f8dbd43f070d
Leonard Frankel <com.leonardfrankel@leonard>
2020-01-31 23:20:09 +1100

    Added main menu scene and main menu code.
    It's a lot more basic than I remember it being. It's a start, I guess.

---
+++ UnityProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+  public string[] levelSceneNames;
+
+  public string menuTitle;
+
+  public const float HEIGHT_OFFSET = 25F;
+  public const float ELEMENT_HEIGHT = 50;
+  public const float ELEMENT_WIDTH = 200;
+  public const float SEPARATION = 5;
+
+  public const string MENU_SCENE = "mainmenu";
+
+  public void OnGUI()
+  {
+    //GUI.skin = skin;
+    
+    float heightOffset = HEIGHT_OFFSET;
+    
+    GUI.Label(new Rect(Screen.width/2 - ELEMENT_WIDTH/2, heightOffset, ELEMENT_WIDTH, ELEMENT_HEIGHT),
+        menuTitle);
+
+    heightOffset += ELEMENT_HEIGHT *2;
+      
+    //god help you if you haven't got the labels and scenes right sized!
+    for(int i = 0; i < levelSceneNames.Length; ++i)
+    {
+      //TODO: change to be percentage of screen rather than absolute position.
+
+      if(GUI.Button(new Rect(Screen.width /2 - ELEMENT_WIDTH/2, heightOffset, ELEMENT_WIDTH, ELEMENT_HEIGHT), levelSceneNames[i]))
+      {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelSceneNames[i].ToLower());
+      }
+        
+      heightOffset += ELEMENT_HEIGHT + SEPARATION;
+        
+    }
+  }
+}
