48f92facdf4dcb67ff4d596d46a1b6d93260210f
Ron <ronaldchau14@gmail.com>
2020-02-01 16:15:05 +1100

    Added changes to tile prefabs and player movement

--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -6,17 +6,39 @@ public class Player : MonoBehaviour
   public float m_rotationDPS = 30;
   public float m_speed = 50;
 
+  private Rigidbody m_body = null;
+
+  private void Start()
+  {
+    m_body = gameObject.GetComponent<Rigidbody>();
+  }
+
   // Fixed update is called in sync with physics
   // Handles movement etc.
   private void FixedUpdate()
   {
     //literally just rotate by the horizontal axis, then add forward by the vertical axis!
-
     float h = Input.GetAxis("Horizontal");
     float v = Input.GetAxis("Vertical");
 
-    transform.Rotate(new Vector3(0, 1, 0), m_rotationDPS * Time.deltaTime * h);
-    transform.position += transform.forward * m_speed * Time.deltaTime * v;
+    // TODO: Too lazy to remove - do it later RON
+    MovePlayer(v, h);
+  }
+
+  public void MovePlayer(float v, float h) 
+  {
+    if(v+h != 0) {
+      Vector3 moveDirection = Vector3.zero;
+      moveDirection.z = v;
+      moveDirection.x = h;
+      moveDirection.Normalize();
+
+      Quaternion rot = Quaternion.RotateTowards(m_body.rotation, 
+                                                Quaternion.LookRotation(moveDirection), 
+                                                Time.deltaTime * m_rotationDPS);
+      m_body.MoveRotation(rot);
+      m_body.MovePosition(m_body.position + moveDirection * m_speed * Time.deltaTime);
+    }
   }
 
 }
