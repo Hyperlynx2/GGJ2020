Hyperlynx2/GGJ2020

commit e9c29970543f707aba9a5b3b6e623ea5e212d7c3

Delay between end of game and showing the end game screen.

UnityProject/Assets/Scripts/GameManager.cs
@@ -26,6 +26,9 @@ public class GameManager : MonoBehaviour
   [Range(0, 1)]
   public float m_cantLeaveYetScale;
 
+  public float m_endGameDelay;
+  private float m_endGameDelayRemaining;
+
   public void Awake()
   {
     if(s_instance != null)
@@ -75,10 +78,14 @@ public class GameManager : MonoBehaviour
     switch(m_gamestate)
     {
       case GAMESTATE.LOST:
-        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), m_youLostTexture);
+        m_endGameDelayRemaining -= Time.deltaTime;
+        if (m_endGameDelayRemaining <= 0)
+          GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), m_youLostTexture);
         break;
       case GAMESTATE.WON:
-        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), m_youWonTexture);
+        //m_endGameDelayRemaining -= Time.deltaTime; // LHF: nah, no need
+        if(m_endGameDelayRemaining <= 0)
+          GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), m_youWonTexture);
         break;
       case GAMESTATE.PLAYING:
         if (m_cantLeaveYetDisplayTimeRemaining > 0)
@@ -132,6 +139,7 @@ public class GameManager : MonoBehaviour
     {
       Debug.Log("You win!");
       m_gamestate = GAMESTATE.WON;
+      m_endGameDelayRemaining = m_endGameDelay;
     }
     else
     {
@@ -144,6 +152,7 @@ public class GameManager : MonoBehaviour
   {
     Debug.Log("Ouch!");
     m_gamestate = GAMESTATE.LOST;
+    m_endGameDelayRemaining = m_endGameDelay;
   }
 
   private List<Trap> m_traps = new List<Trap> { };

UnityProject/Assets/Prefabs/GameManager.prefab
@@ -51,4 +51,5 @@ MonoBehaviour:
   m_cantLeaveYetHorz: 0.08
   m_cantLeaveYetVert: 0.8
   m_cantLeaveYetScale: 0.28
+  m_endGameDelay: 1.5
   m_menuSceneIndex: 1

commit 02a11a218fe1ffcee6c58bd663ffb8a4d4197b31

[bugfix] Issue #25. It was still waiting for a timer before displaying the end game screen, despite not decrementing the timer.
Bug introduced in e9c29970543f707aba9a5b3b6e623ea5e212d7c3.

UnityProject/Assets/Scripts/GameManager.cs
@@ -83,9 +83,7 @@ public class GameManager : MonoBehaviour
           GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), m_youLostTexture);
         break;
       case GAMESTATE.WON:
-        //m_endGameDelayRemaining -= Time.deltaTime; // LHF: nah, no need
-        if(m_endGameDelayRemaining <= 0)
-          GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), m_youWonTexture);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), m_youWonTexture);
         break;
       case GAMESTATE.PLAYING:
         if (m_cantLeaveYetDisplayTimeRemaining > 0)